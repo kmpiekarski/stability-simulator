b5f66030eb163730a74c564010f4ce462f53382f
kmpiekarski <kenneth@kmpiekarski.com>
2022-09-15 14:19:01 -0400

    Setup component to demo ag-grid GridOptions method of rendeirng data to grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,23 @@
-import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+import { Store } from '@ngrx/store';
+import { ColDef, GridReadyEvent, GridOptions } from 'ag-grid-community';
 import { Observable } from 'rxjs';
+import { requestToGetStocks, getStockData } from './stock.actions';
+import { Stock } from './stock.model';
+// import { StockService } from './stock.service';
 
-
-// Component is tasked with presenting data to the user. Getting data (back-end) is for a Service class
-// @Component explicitly declaration this file as a component
 @Component({
   // component metadata
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[/* StockService */]
 })
 
 export class AppComponent {
-  rowData$!: Observable<any[]>;
-  // an Observable is "streams of events and data in real-time" meant to handle asynchronous code. So it is persistenly monitoring if a specific action like a "click" occurs
 
   public colDefs: ColDef[] = [
-    { field: '_id' }, 
+    { field: 'id' }, 
     { field: 'priceHigh' },
     { field: 'priceLow' },
     { field: 'priceDiff' }
@@ -29,9 +28,47 @@ export class AppComponent {
     filter: true,
   }
 
-  constructor(private http: HttpClient) {}
+  public gridOptions: GridOptions;
+
+  constructor(private store: Store) {
+    this.gridOptions = <GridOptions>{
+      rowData: this.createRowData(),
+      columnDefs: this.createColumnDefs(),
+      // enableFilter: true,
+      // onGridReady: () => {}
+    }
+  }
+
+  private createColumnDefs() {
+    return [
+      { headerName: "ID", field: "id" },
+      { headerName: "Price High", field: "priceHigh" },
+      { headerName: "Price Low", field: "priceLow" },
+      { headerName: "Price Difference", field: "priceDiff" }
+    ];
+}
+
+private createRowData() {
+  return [
+    { id: "631c0603efa47fddaaffb9a4", priceHigh: "$1,937.04", priceLow: "$980.68" },
+    { id: "631c060317dc313ac672a8d9", priceHigh: "$1,715.12", priceLow: "$905.16" },
+    { id: "631c060387736aab68db8c87", priceHigh: "$1,363.11", priceLow: "$178.83" },
+    { id: "631c060323ccb028ac614324", priceHigh: "$1,354.27", priceLow: "$457.10" },
+    { id: "631c0603be36245839098cb8", priceHigh: "$1,657.68", priceLow: "$668.40" }
+  ];
+}
+
+  // private createRowData() {
+  //   return this.store.dispatch(requestToGetStocks())
+  // }
+
+
+  // onGridReady(params: GridReadyEvent) {
+  //   // this.store.dispatch(getStockData())
+  // }
 
   ngOnInit() {
-    this.rowData$ = this.http.get<any[]>('https://gist.githubusercontent.com/kmpiekarski/accf7b74bc9d01483a61aa82e9fc7b71/raw/724750c28a81d3bc7eff6c9b2d558e449915115c/hardCodedData.json');
+    this.store.dispatch(requestToGetStocks())
   }
+
 }
